22b7f9bb67851fcb777ca3501f066cd261edf953
ravelonarivo <irinaravelonarivo@gmail.com>
2020-05-11 02:06:49 +0000

    Initial build subtree commit

---
+++ precache-manifest.27b54333747047a63f9ec8ed09e42011.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e69592b575a7df79c19abf33cd10b014",
+    "url": "/monsterfriends/index.html"
+  },
+  {
+    "revision": "ef552da3b839a48e12fe",
+    "url": "/monsterfriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "54befea0ca3f35acdf72",
+    "url": "/monsterfriends/static/css/main.0ebd5955.chunk.css"
+  },
+  {
+    "revision": "ef552da3b839a48e12fe",
+    "url": "/monsterfriends/static/js/2.3b9118b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/monsterfriends/static/js/2.3b9118b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54befea0ca3f35acdf72",
+    "url": "/monsterfriends/static/js/main.2f367083.chunk.js"
+  },
+  {
+    "revision": "4ca6e02fb6efd4e5438b",
+    "url": "/monsterfriends/static/js/runtime-main.96d43ebb.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/monsterfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
